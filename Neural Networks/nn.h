c30a75caaff94963a3e6433cab53458cf2bb39d7
daisemu <daisemu@gmail.com>
2016-10-14 17:30:16 +0300

    Add files via upload

---
+++ Neural Networks/nn.h
@@ -0,0 +1,38 @@
+#ifndef NNCore
+
+class Neuron{
+    public:
+        Neuron(){
+            input = 0;
+            output = 0;
+        }
+
+        void processing(long double treshold,long double val){
+            if(input >= treshold)
+                output = N;   
+        }
+      
+        long double input, output;
+    private:
+};
+
+class Synapse{
+    public:
+        Synapse(Neuron *_a, Neuron *_b, long double def_w = 1){
+            a = _a;
+            b = _b;
+            weight = def_w;
+        }
+ 
+        void processing(){
+            b->input += (a->output * weight);
+            b->processing(0.1,b->input);
+        }
+        
+        long double weight;
+    private:
+        Neuron *a, *b;
+        
+};
+
+#endif
---
+++ Neural Networks/nnshort.h
@@ -0,0 +1,12 @@
+#ifndef using_short_names
+
+#include "nn.h"
+
+#define N Neuron
+#define S Synapse
+#define proc processing
+#define W weight
+#define I input
+#define O output
+
+#endif
